81714217f082209e494a266d9a390f1237493920
ZumaAkbarID <rahmatwahyumaakbar@gmail.com>
2024-01-22 23:32:09 +0700

    Module

---
+++ calculator.go
@@ -0,0 +1,21 @@
+package go_calc
+
+func penambahan(bil1, bil2 int32) int32 {
+	return bil1 + bil2
+}
+
+func pengurangan(bil1, bil2 int32) int32 {
+	return bil1 - bil2
+}
+
+func perkalian(bil1, bil2 int32) int32 {
+	return bil1 * bil2
+}
+
+func pembagian(bil1, bil2 float32) float32 {
+	return bil1 / bil2
+}
+
+func modulo(bil1, bil2 int32) int32 {
+	return bil1 % bil2
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ZumaAkbarID/go-calc
+
+go 1.21.6
